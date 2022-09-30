a96654ca80e038e068e927d4a3492e8108e57230
Jack Hermanson <jack.hermanson@live.com>
2022-09-29 20:51:44 -0600

    it's something

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggleButton = document.getElementById("toggle-button");
+
+function toggleTheme() {
+
+    const cssTag = document.getElementsByTagName('link')[1];
+    const imgTag = document.getElementById("logo");
+
+
+    if (cssTag.getAttribute("href") === "light.css") {
+        cssTag.setAttribute("href", "dark.css");
+        toggleButton.innerText = "Theme Activated";
+        imgTag.setAttribute("src", "dark-mode-logo.png");
+    } else {
+        cssTag.setAttribute("href", "light.css");
+        toggleButton.innerText = "Dark Mode?";
+        imgTag.setAttribute("src", "light-mode-logo.png");
+    }
+
+}
+
+
+toggleButton.addEventListener("click", () => {
+    toggleTheme();
+})
+
